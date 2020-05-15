a41784b5aba3634f015366efa5c2f16a83107d48
Ariel <atelierbump@gmail.com>
2020-05-15 05:47:21 -0500

    Semantics working

--- src/mxsSemantics.ts
+++ src/mxsSemantics.ts
@@ -2,38 +2,20 @@ import * as vscode from 'vscode';
 
 import * as moo from 'moo';
 import maxAPI from './schema/mxsAPI';
-// import { TextDocumentSaveReason } from 'vscode';
-/*
-import {
-    otherprimitives,
-    superclass,
-    systemvars,
-    primitiveclass,
-    generictypes
-} from './schema/mxsKeywords';
-const keywordsDB = {
-    kw_primitive: otherprimitives,
-    kw_superclass: superclass,
-    kw_system: systemvars,
-    kw_class: primitiveclass,
-    kw_types: generictypes
- };
-*/
 //-------------------------------------------------------------------------------------------------------------
 const caseInsensitiveKeywords = (map: { [k: string]: string | string[]; }) => {
     const transform = moo.keywords(map)
     return (text: string) => transform(text.toLowerCase())
 }
 let lexer = moo.compile({
+    // the comments
+    comment_SL: { match: /--.*$/, lineBreaks: false, },
+    comment_BLK: { match: /\/\*(?:.|[\n\r])*?\*\//, lineBreaks: true },
+    // there is a problem with the strings
+    string: { match: /[@]?"(?:\\["\\rn]|[^"])*?"/, lineBreaks: true },
     //WS:      /[ \t]+/,
     //string:  /"(?:\\["\\]|[^\n"\\])*"/,
-    WS: { match: /(?:[ \t]+|(?:[ \t]*?[\\]+[ \t\r\n;]*)+?)/, lineBreaks: true },
-
-    string: [
-        { match: /"(?:\\["\\rn]|[^"\\])*?"/ },
-        { match: /@"[^"\\]*(?:\\.[^"\\]*)*"/ },
-        { match: /"""[^]*?"""/, lineBreaks: true },
-    ],
+    WS: { match: /(?:[ \t]+|(?:[ \t]*?[\\]+[ \t\r\n]*)+?)/, lineBreaks: true },
     // path_name $mounstrosity*/_?
     path: [
         { match: /[$](?:[A-Za-z0-9_\*\?\.\\]*)/ },
@@ -43,17 +25,8 @@ let lexer = moo.compile({
     params: { match: /[A-Za-z_\u00C0-\u00FF][A-Za-z0-9_\u00C0-\u00FF]*[:]/ },
     // param: {match: /\:{1}/},
     global_typed: { match: /::[A-Za-z_\u00C0-\u00FF][A-Za-z0-9_\u00C0-\u00FF]*/ },
-    // IDENTIFIERS
-    // includes special alphanumeric chars
-    identity: {
-        match: /[&-\.]?[A-Za-z_\u00C0-\u00FF][A-Za-z0-9_\u00C0-\u00FF]*/,
-        type: caseInsensitiveKeywords(maxAPI)
-    },
     // a mounstrosity
     typed_iden: { match: /'(?:\\['\\rn]|[^'\\\n])*?'/ },
-    // the comments
-    comment_SL: { match: /--.*$/, lineBreaks: false, },
-    comment_BLK: { match: /\/\*(?:.|[\n\r])*?\*\//, lineBreaks: true },
     // strings ~RESOURCE~
     locale: { match: /~[A-Za-z0-9_]+~/ },
     // WHITESPACE -- Should go higher on the chain??
@@ -88,68 +61,64 @@ let lexer = moo.compile({
     comparison: ['==', '!=', '>', '<', '>=', '<='],
     assign: ['=', '+=', '-=', '*=', '/='],
     math: ['+', '-', '*', '/', '^'],
-    delimiter: ['.', ',', ':'],
+    delimiter: ['.', ',', ':', ';'],
+
+    // IDENTIFIERS
+    // includes special alphanumeric chars
+    identity: {
+        match: /[&-]?[A-Za-z_\u00C0-\u00FF][A-Za-z0-9_\u00C0-\u00FF]*\b/,
+        type: caseInsensitiveKeywords(maxAPI)
+    },
+
     NL: { match: /(?:\r|\r\n|\n)+/, lineBreaks: true },
     // [\$?`] COMPLETE WITH UNWANTED CHARS HERE THAT CAN BREAK THE TOKENIZER
     error: { match: /[¿¡!`]/, error: true },
 })
 //-------------------------------------------------------------------------------------------------------------
-const tokenTypesLegend = [
-    'variable',
-    'type',
-    'function',
-    'class',
-    'superclass',
-    'interface',
-    'struct',
-];
-const tokenTypesSet = new Set<string>(tokenTypesLegend);
-
 const tokenTypes = new Map<string, number>();
 const tokenModifiers = new Map<string, number>();
+const tokenTypesSet = new Set<string>();
 
 export const legend = (function () {
-    tokenTypesLegend.forEach((tokenType, index) => tokenTypes.set(tokenType, index + 1));
-    //*
-    const tokenModifiersLegend = [
-    	'generic', 'primitive', 'system', 'constant', 'readonly', 'deprecated',
+    const tokenTypesLegend = [
+        'comment', 'keyword', 'number', 'regexp', 'operator', 'namespace',
+        'string', 'type', 'struct', 'class', 'interface', 'enum', 'typeParameter', 'function',
+        'member', 'macro', 'variable', 'parameter', 'property', 'label',
     ];
-    tokenModifiersLegend.forEach((tokenModifier, index) => tokenModifiers.set(tokenModifier, index));
-    //*/
-    return new vscode.SemanticTokensLegend(tokenTypesLegend, []);
+
+	tokenTypesLegend.forEach((tokenType, index) => tokenTypes.set(tokenType, index));
+    tokenTypesLegend.forEach(item => tokenTypesSet.add(item));
+
+	const tokenModifiersLegend = [
+		'declaration', 'documentation', 'readonly', 'static', 'abstract', 'deprecated',
+		'modification', 'async'
+	];
+	tokenModifiersLegend.forEach((tokenModifier, index) => tokenModifiers.set(tokenModifier, index));
+
+	return new vscode.SemanticTokensLegend(tokenTypesLegend, tokenModifiersLegend);
 })();
-//-------------------------------------------------------------------------------------------------------------
-/*
-export function activate(context: vscode.ExtensionContext) {
-    context.subscriptions.push(vscode.languages.registerDocumentSemanticTokensProvider({ language: 'maxscript' }, new DocumentSemanticTokensProvider(), legend));
-}
-// */
-//-------------------------------------------------------------------------------------------------------------
+
 interface IParsedToken {
-    line: number;
-    startCharacter: number;
-    length: number;
-    tokenType: string;
-    tokenModifiers: string[];
+	line: number;
+	startCharacter: number;
+	length: number;
+	tokenType: string;
+	tokenModifiers: string[];
 }
-//-------------------------------------------------------------------------------------------------------------
-// class DocumentSemanticTokensProvider {
+
 export class DocumentSemanticTokensProvider implements vscode.DocumentSemanticTokensProvider {
+	async provideDocumentSemanticTokens(document: vscode.TextDocument, token: vscode.CancellationToken): Promise<vscode.SemanticTokens> {
+		const allTokens = this._parseText(document.getText());
 
-    async provideDocumentSemanticTokens(document: vscode.TextDocument, token: vscode.CancellationToken): Promise<vscode.SemanticTokens> {
-        //*
-        const allTokens = this._parseText(document.getText());
-        const builder = new vscode.SemanticTokensBuilder();
-        // /*
-        allTokens.forEach((token) => {
-            builder.push(token.line, token.startCharacter, token.length, this._encodeTokenType(token.tokenType), this._encodeTokenModifiers(token.tokenModifiers));
-        });
-        // */
-        return builder.build();
+		//console.log(allTokens);
 
-        //*/
-    }
-
+		const builder = new vscode.SemanticTokensBuilder();
+		allTokens.forEach((token) => {
+			builder.push(token.line, token.startCharacter, token.length, this._encodeTokenType(token.tokenType), this._encodeTokenModifiers(token.tokenModifiers));
+		});
+		return builder.build();
+	}
+    // This comes directly from the extension-example
 	private _encodeTokenType(tokenType: string): number {
 		if (tokenTypes.has(tokenType)) {
 			return tokenTypes.get(tokenType)!;
@@ -158,7 +127,7 @@ export class DocumentSemanticTokensProvider implements vscode.DocumentSemanticTo
 		}
 		return 0;
 	}
-
+    // This comes directly from the extension-example
 	private _encodeTokenModifiers(strTokenModifiers: string[]): number {
 		let result = 0;
 		for (let i = 0; i < strTokenModifiers.length; i++) {
@@ -171,79 +140,34 @@ export class DocumentSemanticTokensProvider implements vscode.DocumentSemanticTo
 		}
 		return result;
 	}
-    /*
-	private _parseText(text: string): IParsedToken[] {
-		let r: IParsedToken[] = [];
-		let lines = text.split(/\r\n|\r|\n/);
-		for (let i = 0; i < lines.length; i++) {
-			const line = lines[i];
-			let currentOffset = 0;
-			do {
-				const openOffset = line.indexOf('[', currentOffset);
-				if (openOffset === -1) {
-					break;
-				}
-				const closeOffset = line.indexOf(']', openOffset);
-				if (closeOffset === -1) {
-					break;
-				}
-				let tokenData = this._parseTextToken(line.substring(openOffset + 1, closeOffset));
-				r.push({
-					line: i,
-					startCharacter: openOffset + 1,
-					length: closeOffset - openOffset - 1,
-					tokenType: tokenData.tokenType,
-					tokenModifiers: tokenData.tokenModifiers
-				});
-				currentOffset = closeOffset;
-			} while (true);
-		}
-		return r;
-	}
-
-	private _parseTextToken(text: string): { tokenType: string; tokenModifiers: string[]; } {
-		let parts = text.split('.');
-		return {
-			tokenType: parts[0],
-			tokenModifiers: parts.slice(1)
-		};
-    }
-    */
-    private _parseText(text:string) {
+    // using moo for tokenizing the source
+	private _parseText(text:string): IParsedToken[]  {
         let r: IParsedToken[] = [];
 
-        // tokenize source
-        // let tokenizedText = this._tokenize(text);
-        // filter tokens
-        // let validTokens = this._filterTokens(tokenizedText);
         let getTokens = this._tokenize(text);
 
         getTokens.forEach(token => r.push({
-            line: token.line,
-            startCharacter: token.offset - 1,
-            length: token.offset + (token.text.length - 1),
+            line: token.line - 1,
+            startCharacter: token.col - 1,
+            length: (token.text.length),
             tokenType: token.type,
             tokenModifiers: []
         }));
         return r;
-    }
+	}
 
-    private _filterTokens(tokens: any[]) {
-        return tokens.filter(token => tokenTypesSet.has(token.type));
-    }
-
-    private _tokenize(text: string) {
+	private _tokenize(text: string):any[] {
         try {
             // feed the tokenizer
             lexer.reset(text);
 
-            let token: moo.Token;
+            let _token;
             let toks = [];
 
-            while (token = lexer.next()) {
+            while (_token = lexer.next()) {
                 // filter tokens here
-                if (tokenTypesSet.has(token.type)) {
-                    toks.push(token);
+                if (tokenTypesSet.has(_token.type!)) {
+                    toks.push(_token);
                 }
             }
             return toks;
@@ -253,47 +177,3 @@ export class DocumentSemanticTokensProvider implements vscode.DocumentSemanticTo
         }
     }
 }
-/*
-function tokenize(text: string) {
-    try {
-        // feed the tokenizer
-        lexer.reset(text);
-
-        let token: moo.Token;
-        let toks = [];
-
-        while (token = lexer.next()) {
-            // filter tokens here
-            if (tokenTypesSet.has(token.type)) {
-                toks.push(token);
-            }
-            //TODO: Catch tokenizer errors.
-        }
-        return toks;
-    } catch (err) {
-        // just rethrow...
-        throw err;
-    }
-}
-
-function parseText(text: string): IParsedToken[] {
-    let r: IParsedToken[] = [];
-
-    // tokenize source
-    let tokenizedText = tokenize(text);
-    // filter tokens
-    // let validTokens = tokenizedText.filter(token => tokenTypesSet.has(token.type));
-
-    tokenizedText.forEach(token => r.push({
-        line: token.line,
-        startCharacter: token.offset - 1,
-        length: token.offset + (token.text.length - 1),
-        tokenType: token.type,
-        tokenModifiers: []
-    }));
-    return r;
-}
-
-let res = parseText('some abs script');
-console.log(res);
-//*/
