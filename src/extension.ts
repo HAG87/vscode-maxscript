aaed9af16fea9f4ce239d1873974408f07211e3d
Atelier Bump <ariel.g87@gmail.com>
2020-06-20 16:35:55 -0500

    Enabled features in untitled document# Conflicts:#	src/extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -6,7 +6,7 @@ import mxsOutline from './mxsOutline';
 import mxsDefinitions from './mxsDefinitions';
 import { DocumentSemanticTokensProvider, legend } from './mxsSemantics';
 
-export const MXS_MODE: vscode.DocumentFilter = { scheme: 'file', language: 'maxscript' };
+export const MXS_DOC: vscode.DocumentFilter = { scheme: 'file', language: 'maxscript' };
 // this is implemented in contributes languaje-configuration.json
 export const LANG_CFG: vscode.LanguageConfiguration = {
 	indentationRules: {
@@ -25,8 +25,8 @@ let help_addr: string = mxsConfig.get('helpprovider', 'http://help.autodesk.com/
  * MaxScript online help launch at current selected word
  * @param help_addr Addess of the help page
  */
-export async function msxHelp(help_addr: string) {
-	let query = getTextSel();
+export async function msxHelp(textEditor: vscode.TextEditor, help_addr: string) {
+	let query = getTextSel(textEditor);
 	// if (query) {
 	let uri = vscode.Uri.parse(encodeURI(`${help_addr}?query=${query!}&cg=Scripting%20%26%20Customization`));
 	await vscode.commands.executeCommand('vscode.open', uri);
@@ -40,24 +40,24 @@ export async function msxHelp(help_addr: string) {
  */
 export function activate(context: vscode.ExtensionContext) {
 
-	vscode.languages.setLanguageConfiguration(MXS_MODE.language!, LANG_CFG);
+	vscode.languages.setLanguageConfiguration(MXS_DOC.language!, LANG_CFG);
 	// MaxScript Help command
-	context.subscriptions.push(vscode.commands.registerCommand('mxs.help', () => { msxHelp(help_addr); }));
+	context.subscriptions.push(vscode.commands.registerTextEditorCommand('mxs.help', (textEditor) => { msxHelp(textEditor, help_addr); }));
 	// completions
 	if (mxsConfig.get('completions', true)) {
-		context.subscriptions.push(vscode.languages.registerCompletionItemProvider(MXS_MODE, new mxsCompletion(), '.'));
+		context.subscriptions.push(vscode.languages.registerCompletionItemProvider(MXS_DOC.language!, new mxsCompletion(), '.'));
 	}
 	// outliner
 	if (mxsConfig.get('gotosymbol', true)) {
-		context.subscriptions.push(vscode.languages.registerDocumentSymbolProvider(MXS_MODE, new mxsOutline()));
+		context.subscriptions.push(vscode.languages.registerDocumentSymbolProvider(MXS_DOC, new mxsOutline()));
 	}
 	// definition provider
 	if (mxsConfig.get('gotodefinition', true) && mxsConfig.get('gotosymbol', true)) {
-		context.subscriptions.push(vscode.languages.registerDefinitionProvider(MXS_MODE, new mxsDefinitions()));
+		context.subscriptions.push(vscode.languages.registerDefinitionProvider(MXS_DOC, new mxsDefinitions()));
 	}
 	// semantics
 	if (mxsConfig.get('semantics', true)) {
-		context.subscriptions.push(vscode.languages.registerDocumentSemanticTokensProvider(MXS_MODE, new DocumentSemanticTokensProvider(), legend));
+		context.subscriptions.push(vscode.languages.registerDocumentSemanticTokensProvider(MXS_DOC.language!, new DocumentSemanticTokensProvider(), legend));
 	}
 }
 // this method is called when your extension is deactivated
